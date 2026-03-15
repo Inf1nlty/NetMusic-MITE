6040a4362640465cb4056ddc8393a42eed33a846
Inf1nlty <60602644+Inf1nlty@users.noreply.github.com>
2026-03-16 01:32:20 +0800

    1

--- src/main/java/com/github/tartaricacid/netmusic/item/ItemSimpleNetMusic.java
+++ src/main/java/com/github/tartaricacid/netmusic/item/ItemSimpleNetMusic.java
@@ -4,9 +4,9 @@ import net.minecraft.Item;
 import net.xiaoyu233.fml.reload.utils.IdUtil;
 
 public class ItemSimpleNetMusic extends Item {
+
     public ItemSimpleNetMusic(String texture) {
         super(IdUtil.getNextItemID(), texture);
         this.setUnlocalizedName("netmusic." + texture);
     }
 }
-

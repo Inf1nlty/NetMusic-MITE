dfbd601c9438bf7e404c1de8b2591d0fc1e51c26
Inf1nlty <60602644+Inf1nlty@users.noreply.github.com>
2026-03-17 19:05:54 +0800

    1

--- src/main/java/com/github/tartaricacid/netmusic/api/NetEaseMusic.java
+++ src/main/java/com/github/tartaricacid/netmusic/api/NetEaseMusic.java
@@ -28,7 +28,7 @@ public class NetEaseMusic {
     }
 
     public static String getUserAgent() {
-        return StringUtils.joinWith("\u0020",
+        return String.join("\u0020",
                 "Mozilla/5.0 (Windows NT 6.1; Win64; x64)",
                 "AppleWebKit/537.36 (KHTML, like Gecko)",
                 "Chrome/81.0.4044.138",
